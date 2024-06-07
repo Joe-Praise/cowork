99b7d1954fa3c8939af8f3b2ed20d2daec5a9435
Joe Praise <alimipraisejoe@gmail.com>
2024-06-07 13:33:27 +0100

    Initializing project

---
+++ constants/index.ts
@@ -0,0 +1,52 @@
+/* write all conatants here e.g  links or array to be looped
+const navLinks = [
+ {
+    imgURL: "/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/icons/dollar-circle.svg",
+    route: "/my-banks",
+    label: "My Banks",
+  },
+  {
+    imgURL: "/icons/transaction.svg",
+    route: "/transaction-history",
+    label: "Transaction History",
+  },
+  {
+    imgURL: "/icons/money-send.svg",
+    route: "/payment-transfer",
+    label: "Transfer Funds",
+  },
+]
+
+*/
+
+export const navLinks = [
+	{
+		route: '/about',
+		label: 'About',
+	},
+	{
+		route: '/pricing',
+		label: 'Pricing',
+	},
+	{
+		route: '/blog',
+		label: 'Blog',
+	},
+	{
+		route: '/contact',
+		label: 'Contact',
+	},
+	{
+		route: '/login',
+		label: 'Login',
+	},
+	{
+		route: '/sign-up',
+		label: 'Sign up',
+	},
+];
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+
+/* Declare types for e.g 
+declare type SignUpParams = {
+  firstName: string;
+  lastName: string;
+  address1: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  dateOfBirth: string;
+  ssn: string;
+  email: string;
+  password: string;
+};
+*/
