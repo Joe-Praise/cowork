1bb7ef0486dd75fa95f5b4d952596c66a93d6022
Joe Praise <alimipraisejoe@gmail.com>
2024-06-08 18:45:15 +0100

    Implement setup update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 function App() {
 	return (
-		<h1 className='text-3xl font-bold underline text-center'>Hello world!</h1>
+		<>
+			<div className='w-[30%] border-black mx-auto bg-red-600'>
+				<h1 className='text-6xl uppercase underline'>
+					Evaluate your work space with cowork
+				</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque,
+					libero vitae quas, quibusdam aliquam aliquid id vero est cumque harum
+					non doloribus minima dolorum temporibus voluptatibus totam itaque
+					deleniti dolores ratione debitis! Ducimus, consectetur velit.
+					Excepturi recusandae hic inventore tenetur.
+				</p>
+			</div>
+		</>
 	);
 }
 
